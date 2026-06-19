462babf5cecb418041363ecb43c90b43ab295d7f
abdumannon <abdumannonabdukarimov77@gmail.com>
2026-06-19 22:04:16 +0500

    amalyot 3-version

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ toggel_menu.addEventListener("click", function(){
     toggel_menu.classList.toggle("active")
 })
 
+let navigatsya = document.querySelector(".navigatsya")
 let bady = document.querySelector(".bady")
 let toggol = document.querySelector(".toggol")
+let main_4_div = document.querySelector(".main_4_div")
+let main_5_div = document.querySelector(".main_5_div")
+let main_6_div = document.querySelector(".main_6_div")
+let main_7_div = document.querySelector(".main_7_div")
+let main_8_div = document.querySelector(".main_8_div")
 
 toggol.addEventListener("click", function(){
     bady.classList.toggle("active")
+    navigatsya.classList.toggle("active")
     toggol.classList.toggle("active")
+    main_4_div.classList.toggle("active")
+    main_5_div.classList.toggle("active")
+    main_6_div.classList.toggle("active")
+    main_7_div.classList.toggle("active")
+    main_8_div.classList.toggle("active")
     
 })
 
